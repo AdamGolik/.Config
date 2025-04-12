f7b89c02fbe98136a8a76a9af5f7dcba1fc6edcd
AdamGolik <adamgolik031@gmail.com>
2025-04-12 23:13:42 +0200

    dodanie colorsheme lepsze

--- lua/josean/plugins/colorscheme.lua
+++ lua/josean/plugins/colorscheme.lua
@@ -1,30 +1,51 @@
 local themes = {
+  "catppuccin",
   "dracula",
   "rose-pine",
-  "catppuccin",
   "tokyonight",
   "everforest",
+  "gruvbox",
+  "nord",
+  "nightfox",
+  "kanagawa",
 }
 
+-- Indeks domyślnego motywu (catppuccin)
 local current = 1
 
+-- Komenda do przełączania motywu
 vim.api.nvim_create_user_command("ToggleTheme", function()
   current = current % #themes + 1
   vim.cmd("colorscheme " .. themes[current])
 end, {})
 
+-- Skrót klawiszowy
 vim.keymap.set("n", "<leader>th", "<cmd>ToggleTheme<CR>", { desc = "Toggle Theme" })
 
 return {
-  -- Dracula (różowo-fioletowy)
+  -- Catppuccin
   {
-    "Mofiqul/dracula.nvim",
+    "catppuccin/nvim",
+    name = "catppuccin",
     lazy = false,
     priority = 1000,
     config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+      })
+      vim.cmd.colorscheme("catppuccin") -- <-- ustawienie motywu PO załadowaniu pluginu
+    end,
+  },
+
+  -- Dracula
+  {
+    "Mofiqul/dracula.nvim",
+    lazy = true,
+    config = function()
       require("dracula").setup({
         transparent_bg = true,
-        overrides = function(colors)
+        overrides = function()
           return {
             Normal = { bg = "NONE" },
             NormalFloat = { bg = "NONE" },
@@ -36,32 +57,27 @@ return {
     end,
   },
 
-  -- Rose Pine (pastelowy róż)
+  -- Rose Pine
   {
     "rose-pine/neovim",
     name = "rose-pine",
+    lazy = true,
     config = function()
       require("rose-pine").setup({
         disable_background = true,
+        disable_float_background = true,
+        highlight_groups = {
+          Normal = { bg = "NONE" },
+          NormalFloat = { bg = "NONE" },
+        },
       })
     end,
   },
 
-  -- Catppuccin (styl pastelowy z profilami)
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha", -- mocha = dark
-        transparent_background = true,
-      })
-    end,
-  },
-
-  -- TokyoNight (ładny niebieski)
+  -- TokyoNight
   {
     "folke/tokyonight.nvim",
+    lazy = true,
     config = function()
       require("tokyonight").setup({
         style = "storm",
@@ -70,12 +86,74 @@ return {
     end,
   },
 
-  -- Everforest (zielony, miękki)
+  -- Everforest
   {
     "sainnhe/everforest",
+    lazy = true,
     config = function()
       vim.g.everforest_background = "soft"
       vim.g.everforest_transparent_background = 2
+    end,
+  },
+
+  -- Gruvbox
+  {
+    "morhetz/gruvbox",
+    lazy = true,
+    config = function()
+      vim.g.gruvbox_contrast_dark = "soft"
+      vim.cmd([[
+        hi Normal guibg=NONE ctermbg=NONE
+        hi NormalNC guibg=NONE ctermbg=NONE
+        hi NormalFloat guibg=NONE ctermbg=NONE
+        hi EndOfBuffer guibg=NONE ctermbg=NONE
+      ]])
+    end,
+  },
+
+  -- Nord
+  {
+    "shaunsingh/nord.nvim",
+    lazy = true,
+    config = function()
+      vim.g.nord_contrast = true
+      vim.g.nord_borders = true
+      vim.cmd([[
+        hi Normal guibg=NONE ctermbg=NONE
+        hi NormalFloat guibg=NONE ctermbg=NONE
+        hi EndOfBuffer guibg=NONE ctermbg=NONE
+      ]])
+    end,
+  },
+
+  -- Nightfox
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = true,
+    config = function()
+      require("nightfox").setup({
+        options = {
+          transparent = true,
+        },
+      })
+    end,
+  },
+
+  -- Kanagawa
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    config = function()
+      require("kanagawa").setup({
+        transparent = true,
+        overrides = function()
+          return {
+            Normal = { bg = "NONE" },
+            NormalFloat = { bg = "NONE" },
+            FloatBorder = { bg = "NONE" },
+          }
+        end,
+      })
     end,
   },
 }
